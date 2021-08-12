49604765f2383f0bdffa41a0ddbdeac8af9dffbe
Thiago Henrique Vicentini <thiago-650@usp.br>
2021-08-12 16:17:38 -0300

    Atualizando cards

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-const fetchPokemon = () => {
-    const getPokemonURL = (id) => `https://pokeapi.co/api/v2/pokemon/${id}`
-    
+const getPokemonURL = (id) => `https://pokeapi.co/api/v2/pokemon/${id}`
+
+const fetchPokemon = () => {    
     const pokemonPromises = []
 
     for(let i=1; i<=150; i++){
@@ -15,8 +15,8 @@ const fetchPokemon = () => {
                 const types = pokemon.types.map(typeInfo => typeInfo.type.name)
 
                 accumulator += `
-                <li class="card">
-                    <img class="card-image ${types[0]}" alt="${pokemon.name}" src="${pokemon.sprites.front_default}" />
+                <li class="card ${types[0]}">
+                    <img class="card-image" alt="${pokemon.name}" src="${pokemon.sprites.front_default}" />
                     <h2 class="card-title">${pokemon.id} ${pokemon.name} </h2>
                     <p class="card-subtitle"> ${types.join(' | ')}</p>
                 </li>
