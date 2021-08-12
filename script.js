bdab250ce4df279ec554b731f5ac59e2f6803f8d
Thiago Henrique Vicentini <thiago-650@usp.br>
2021-08-11 22:52:21 -0300

    Adicionando modificações

---
+++ script.js
@@ -0,0 +1,35 @@
+const fetchPokemon = () => {
+    const getPokemonURL = (id) => `https://pokeapi.co/api/v2/pokemon/${id}`
+    
+    const pokemonPromises = []
+
+    for(let i=1; i<=150; i++){
+        pokemonPromises.push(fetch(getPokemonURL(i)).then(response => response.json()))
+    }
+
+    Promise.all(pokemonPromises)
+        .then(pokemons => {
+            //console.log(pokemons)
+
+            const lisPokemons = pokemons.reduce((accumulator, pokemon) => {
+                const types = pokemon.types.map(typeInfo => typeInfo.type.name)
+
+                accumulator += `
+                <li class="card">
+                    <img class="card-image ${types[0]}" alt="${pokemon.name}" src="${pokemon.sprites.front_default}" />
+                    <h2 class="card-title">${pokemon.id} ${pokemon.name} </h2>
+                    <p class="card-subtitle"> ${types.join(' | ')}</p>
+                </li>
+                `
+                return accumulator
+            }, '')
+
+            const ul = document.querySelector('[data-js="pokedex"]')
+
+            ul.innerHTML = lisPokemons       
+            
+            console.log(lisPokemons)
+        })
+}
+
+fetchPokemon()
