a38823eee35169f488437033a775262b227ee85d
Thiago Henrique Vicentini <thiago-650@usp.br>
2021-08-13 23:39:18 -0300

    Salvando progresso

--- cookies.js
+++ cookies.js
@@ -12,9 +12,54 @@ if(id_pokemon != null){
     pokemonPromise.then(pokemon => {
         const types = pokemon.types.map(typeInfo => typeInfo.type.name)
         const accumulator = `
-            <img class="card-image" alt="${pokemon.name}" src="${pokemon.sprites.front_default}" />
-            <h2 class="card-title">${pokemon.id} ${pokemon.name} </h2>
-            <p class="card-subtitle"> ${types.join(' | ')}</p>
+            <div class="pokeinfos">
+                <div class="infos">    
+                    <div class="namenum">
+                        <span id="name">${pokemon.name}</span>
+                        <span id="num">N° ${pokemon.id}</span>
+                    </div>
+
+                    <div class="definition">
+                        <div><p>${pokemon.name}</p></div>
+                    </div>
+
+                    <div class="type">
+                        <p>Type</p>
+                        <div class="each-type">
+                            <div class="style-infos"><p>${types[0]}</p></div>
+                            <div class="style-infos"><p>Type 2</p></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="pokeimg">
+                    <img src="${pokemon.sprites.front_default}" alt="pokemon" width="739" height="598"/>
+                </div>
+            </div>
+            
+            <div class="quadro">
+                <div class="height">
+                    Height: ${pokemon.height}
+                </div>
+                <div class="weight">
+                    Weight: ${pokemon.weight}
+                </div>
+                <div class="abilities">
+                    Abilities
+                </div>
+            </div>
+
+            <div class = "circles">
+                <div class="round-frame-default"></div>
+                <span class="arrow"><i class= "material-icons arrow">east</i></span>
+                <div class="round-frame-default"></div>
+                <span class="arrow"><i class= "material-icons arrow">east</i></span>
+                <div class="round-frame-default"></div>
+            </div>
+            
+            <div class= "buttons">
+                <button class= "button-style" id="button-left"><i class= "material-icons">chevron_left</i></button>
+                <button class= "button-style" id="button-right"><i class= "material-icons">chevron_right</i></button>
+            </div>
         `
         const div = document.querySelector('[data-js="pokedex"]')
         div.innerHTML = accumulator      
